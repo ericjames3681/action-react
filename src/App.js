c5fdff1477542a9d3301c9746d801ee688d76be6
apple <apple@MacBook.home>
2020-06-15 09:42:03 -0500

    search bar...

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">E R I C ' S &nbsp; M O V I E S</h1>
+        <h1 className="title">E R I C ' S</h1>
+        <h1 className="title">M O V I E S</h1>
+        <br />
         <SearchMovies />
       </div>
     );
