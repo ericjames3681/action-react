275f84fbd6ec4d03c5759b07cf7a9117e05084ed
apple <apple@MacBook.home>
2020-06-16 16:50:09 -0500

    displaying movie cards...

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1 className="title">E R I C ' S</h1>
-        <h1 className="title">M O V I E S</h1>
+        <h1 className="title">A C T I O N !</h1>
+
         <br />
         <SearchMovies />
       </div>
--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -2,36 +2,61 @@ import React, { useState } from "react";
 
 export default function SearchMovies() {
   const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (e) => {
     e.preventDefault();
-    console.log("submitting...");
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=81658f5acccfefd659f27ef7b3ee3c9d&language=en-US&query=${query}&page=1&include_adult=false`;
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        placeholder="ex: Jurassic Park"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="ex: Star Wars"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card">
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + " poster"}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p>
+                  <small>RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p>
+                  <small>RATING: {movie.vote_average}</small>
+                </p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
