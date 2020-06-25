516e6629cb6d9ca7c0cf53b872faac7232d9d82e
apple <apple@MacBook.home>
2020-06-25 11:52:46 -0500

    hide API_KEY...

--- src/SearchMovies.jsx
+++ src/SearchMovies.jsx
@@ -6,11 +6,11 @@ import "./App.scss";
 export default function SearchMovies() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-
+  const API_KEY = `${process.env.REACT_APP_API_KEY}`;
   const searchMovies = async (e) => {
     e.preventDefault();
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=81658f5acccfefd659f27ef7b3ee3c9d&language=en-US&query=${query}&page=1&include_adult=false`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
     try {
       const res = await fetch(url);
       const data = await res.json();
